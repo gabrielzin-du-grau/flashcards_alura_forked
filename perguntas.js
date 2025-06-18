89c591b5ac0cd910bc83e68c80d4a56345e73520
gabrielzin-du-grau <wurmeister.gabriel@escola.pr.gov.br>
2025-06-18 01:23:18 +0000

    fdfdfdfd

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,63 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'conecimentos gerais',
+    'qual é o maior planeta do sistema solar?',
+    'é Jupiter'
+)
+
+criaCartao(
+    'história',
+    'quem foi o primeiro imperador do Brasil',
+    'Dom Pedro I'
+)
+
+criaCartao(
+    'Português',
+    'Qual é o sujeito na frase: "O gato perseguiu o rato?',
+    'O gato'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual o maior oceano do mundo?',
+    'O Oceano Pacífico'
 )
-
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'lingua inglesa',
+    'What is the past tense of the verb "to go?',
+    'Went'
 )
-
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Conhecimentos gerais',
+    'Quantos ossos tem o corpo humano adulto?',
+    '206 ossos'
+)
+criaCartao(
+    'conhecimentos gerais',
+    'Qual é o metal mais abundante na crosta terrestre?',
+    'Aluminio'
+)
+criaCartao(
+    'jogos',
+    'Qual jogo eletrônico tem um personagem encanador famoso que salva princesas?',
+    'O Super mario'
+)
+criaCartao(
+    'filmes',
+    'Qual filme conta a história de um leão chamado Simba?',
+    'O rei leão'
+)
+criaCartao(
+    'HQs',
+    'Qual é o nome real do super-herói Batman?',
+    'Bruce Wayne'
+)
+criaCartao(
+    'matemática',
+    'quanto é 7x8?',
+    '56'
+)
+criaCartao(
+    'biologia',
+    'Qual é o processo pelo qual as plantas produzem seu próprio alimento usando a luz solar?',
+    'Fotossíntese'
 )
